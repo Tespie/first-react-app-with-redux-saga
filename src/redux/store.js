0dae92f067a3e7d67f233bd161cf770786d605f6
Tejas Patel <tejaspatel8997@gmail.com>
2024-01-08 19:08:04 +0530

    configure saga in store and just created productSaga with empty generated fn

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,22 @@
 // import { createStore } from "redux";
-// import { legacy_createStore as createStore } from "redux";
 import rootReducer from "./rootReducer";
 import { configureStore } from "@reduxjs/toolkit";
 
+// imported productSaga
+import productSaga from "./productSaga";
+import createSagaMiddleware from 'redux-saga'
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+
 // const store = createStore(rootReducer);  
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: () => [sagaMiddleware]
+});
+
+// then run the productSaga
+sagaMiddleware.run(productSaga);
 
 export default store;
